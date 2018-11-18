36c0a8841a4f8adad2c7e0c3610bc93702dd8e17
Bob Jeltes <bobjeltes@hotmail.com>
2018-11-18 21:37:58 +0100

    Start for Unity mockup UI

---
+++ GDV-Blok2-Tool-NielsV-BobJ-UnityProj/Assets/TestUIScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestUIScript : MonoBehaviour {
+
+    public string String1_English;
+    public string String1_Dutch;
+    public string String2_English;
+    public string String2_Dutch;
+    public string String3_English;
+    public string String3_Dutch;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
