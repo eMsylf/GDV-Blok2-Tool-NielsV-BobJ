fd835f48f3a015ea0914dbcaeac90eeed1cfdbdf
NVriezen <36536400+NVriezen@users.noreply.github.com>
2018-12-13 12:49:43 +0100

    added features enter

---
+++ GDV-Blok2-Tool-NielsV-BobJ-UnityProj/Assets/Editor/LocalizationTool/LocalizationManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalizationManager : MonoBehaviour {
+
+    public static DataManager data;
+
+    static string selectedLanguage;
+    static string selectedDialog;
+    int selectPopLanguage = 0;
+    int selectPopDialog = 0;
+    public static int totalDialog = 3;
+    string translatedText = "";
+    public static List<string> options = new List<string>();
+
+ public static string GetDialog()
+    {
+        return "";
+    }
+
+    public static bool SetDialog()
+    {
+    return true;
+    }
+
+    public static void SetLanguage()
+    {
+
+    }
+
+    public static void SetAll()
+    {
+        //Get language and dialog if there are any, otherwise make new one
+        //selectedLanguage = data.languages.Keys.FirstOrDefault();
+        //Debug.Log("not empty");
+
+        //Dictionary<string, string> outLang;
+        //if (selectedLanguage != null && data.languages.TryGetValue(selectedLanguage, out outLang))
+        //{
+        //    selectedDialog = outLang.Keys.FirstOrDefault();
+        //}
+        //else
+        //{
+        //    selectedDialog = "";
+        //}
+    }
+
+    public static void NextDialog()
+    {
+        //Debug.Log("Next Text");
+        //selectedDialog = popDialog[(selectPopDialog + 1) % (totalDialog - 2)];
+    }
+
+    public static void PreviousDialog()
+    {
+        //Debug.Log("Previous Text");
+        //selectedDialog = popDialog[(selectPopDialog - 1 + totalDialog - 2) % (totalDialog - 2)];
+        //Debug.Log(selectedDialog);
+    }
+
+    public static void AddLang()
+    {
+
+    }
+}
